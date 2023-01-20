85dc22d051ca6c82d10fcfd18fa7ab4544931751
adeline721933 <122544047+adeline721933@users.noreply.github.com>
2023-01-20 09:46:09 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,146 +1,166 @@
 input.onButtonPressed(Button.A, function () {
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        . # . # .
-        . . # . .
-        . . # . .
-        `)
-    basic.showLeds(`
-        . . . . .
-        . . # . .
-        . # # # .
-        . . # . .
-        . . # . .
-        `)
-    basic.showLeds(`
-        . # . # .
-        . . # . .
-        . # # # .
-        . . # . .
-        . . # . .
-        `)
+    for (let index = 0; index < 4; index++) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . # . # .
+            . . # . .
+            . . # . .
+            `, 50)
+basic.showLeds(`
+            . . . . .
+            . . # . .
+            . # # # .
+            . . # . .
+            . . # . .
+            `, 50)
+basic.showLeds(`
+            . # . # .
+            . . # . .
+            . # # # .
+            . . # . .
+            . . # . .
+            `, 50)
+    }
 })
 input.onButtonPressed(Button.AB, function () {
-    music.startMelody(music.builtInMelody(Melodies.PowerUp), MelodyOptions.ForeverInBackground)
+    music.startMelody(music.builtInMelody(Melodies.PowerUp), MelodyOptions.OnceInBackground)
     basic.showLeds(`
         . . . . .
         . . # . .
         . . # . .
         . . # . .
         . # . # .
-        `)
-    basic.showLeds(`
+        `, 50)
+basic.showLeds(`
         . . # . .
         . . # . .
         . . # . .
         . # # # .
         # # # # #
-        `)
-    basic.showLeds(`
+        `, 50)
+basic.showLeds(`
         . . # . .
         . . # . .
         . # # # .
         # # # # .
         # # # # #
-        `)
-    basic.showLeds(`
+        `, 50)
+basic.showLeds(`
         . . # . .
         . # # # .
         # # # # #
         # # # # .
         # # # # #
-        `)
-    basic.showLeds(`
+        `, 50)
+basic.showLeds(`
         # # # # #
         # # # # #
         # # # # #
         # # # # #
         # # # # #
-        `)
+        `, 50)
 })
 input.onButtonPressed(Button.B, function () {
-    basic.showLeds(`
-        . # . # .
-        . . # . .
-        . # # # .
-        . . # . .
-        . . # . .
-        `)
-    basic.showLeds(`
-        . . . . .
-        . # # . .
-        # # # # .
-        . # . # .
-        . . . # .
-        `)
-    basic.showLeds(`
-        . . . . .
-        . . # . .
-        # # . # .
-        . # . # .
-        . . . # .
-        `)
-    basic.showLeds(`
-        . . . . .
-        . . # . .
-        . # . # .
-        . # . # .
-        . . . # .
-        `)
-    basic.showLeds(`
-        . . . . .
-        . . # . .
-        . # . # .
-        . . . # .
-        . . . # .
-        `)
-    basic.showLeds(`
-        . . . . .
-        . . # . .
-        . . . # .
-        . . . # .
-        . . . # .
-        `)
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . . # .
-        . . . # .
-        . . . # .
-        `)
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . # .
-        . . . # .
-        `)
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . # .
-        `)
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        `)
+    for (let index = 0; index < 4; index++) {
+        basic.showLeds(`
+            . # . # .
+            . . # . .
+            . # # # .
+            . . # . .
+            . . # . .
+            `, 50)
+basic.showLeds(`
+            . . . . .
+            . # # . .
+            # # # # .
+            . # . # .
+            . . . # .
+            `, 50)
+basic.showLeds(`
+            . . . . .
+            . . # . .
+            # # . # .
+            . # . # .
+            . . . # .
+            `, 50)
+basic.showLeds(`
+            . . . . .
+            . . # . .
+            . # . # .
+            . # . # .
+            . . . # .
+            `, 50)
+basic.showLeds(`
+            . . . . .
+            . . # . .
+            . # . # .
+            . . . # .
+            . . . # .
+            `, 50)
+basic.showLeds(`
+            . . . . .
+            . . # . .
+            . . . # .
+            . . . # .
+            . . . # .
+            `, 50)
+basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . # .
+            . . . # .
+            . . . # .
+            `, 50)
+basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . # .
+            . . . # .
+            `, 50)
+basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . # .
+            `, 50)
+basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `, 50)
+    }
 })
 input.onGesture(Gesture.Shake, function () {
-    basic.showNumber(input.compassHeading())
+    basic.showArrow(ArrowNames.North)
 })
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
-    music.playMelody("C5 C5 C5 C5 C5 C5 C5 C5 ", 120)
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . # . .
-        . . . . .
-        . . . . .
-        `)
+    for (let index = 0; index < 10; index++) {
+        basic.showString("A", 5)
+basic.showString("B", 5)
+basic.showString("C", 5)
+basic.showString("D", 5)
+    }
+    Letter = randint(1, 4)
+    if (Letter == 1) {
+        basic.showString("A")
+    } else if (Letter == 2) {
+        basic.showString("B")
+    } else if (Letter == 3) {
+        basic.showString("C")
+    } else if (Letter == 4) {
+        basic.showString("D")
+    }
 })
+let Letter = 0
+basic.showLeds(`
+    . . . . .
+    . # . # .
+    . . . . .
+    # . . . #
+    . # # # .
+    `)
